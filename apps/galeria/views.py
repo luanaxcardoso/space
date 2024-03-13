374941179ac4ff543c013470ea4483bad163da24
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-12 23:37:37 -0300

    atualização - editar e deletar

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -49,9 +49,27 @@ def nova_imagem(request):
     
     return render(request, 'galeria/nova_imagem.html', {'form': form})
     
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
+    
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Imagem editada com sucesso.')
+            return redirect('index')
+    
+    return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
 
-def deletar_imagem(request):
-    pass
+
+
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Deleção feita com sucesso!')
+    return redirect('index')  
+
+
+    
 
--- setup/settings.py
+++ setup/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path, os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-bdazd%c!pzu2)p5x(hm_c@&9!t_s_16r*+-kt83_(2j5r^@vou"
+SECRET_KEY = "django-insecure-#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
