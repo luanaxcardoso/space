3d74c074a3a5b9bfc1a0d149b9ec81230d97fe56
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-03-27 21:08:59 -0300

    feat: atualização url de usuários

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from apps.usuarios.views import login, cadastro, logout
+from apps.usuarios.views import *
 
 urlpatterns = [
-    path('login', login, name='login'),
-    path('cadastro', cadastro, name='cadastro'),
-    path('logout', logout, name='logout'),
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
+    path('logout/', logout, name='logout'),
 ]
